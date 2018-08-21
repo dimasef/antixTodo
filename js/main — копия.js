4510729cae11e42c8dde1bde0061dac6f2a4ba8d
Dima Efimov <dimaefimov@MacBook-Pro-Dima.local>
2018-08-21 22:22:12 +0300

    temporary material files

---
+++ js/main — копия.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.4/css/materialize.min.css">
+
+</head>
+
+<body> 
+<div class="container">
+<h5>A Demo of Materialize Switch</h5>
+<!-- Switch -->
+  <div class="switch">
+    <label>
+      I disagree
+      <input type="checkbox">
+      <span class="lever"></span>
+      I agree
+    </label>
+  </div>
+
+
+<h5>Checked Initially</h5>
+  <div class="switch">
+    <label>
+      No
+      <input type="checkbox" checked>
+      <span class="lever"></span>
+      Yes
+    </label>
+  </div>
+</div>
+
+</body>   
+</html>
